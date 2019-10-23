077ccba1a796a8b9d67d35d0466f2e3afd375f93
Hari Kunwar <harikunwar@gmail.com>
2019-10-22 20:07:28 -0700

    Add template xcode project.

---
+++ SwiftUIBasic/SwiftUIBasic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIBasic
+//
+//  Created by Kunwar, Hari on 10/22/19.
+//  Copyright © 2019 Kunwar, Hari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
